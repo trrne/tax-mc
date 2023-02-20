772d02cbf5708c2792c7f9538ce933a1d062aadd
cet <cets021@gmail.com>
2023-02-21 00:19:13 +0900

    remove: dir

--- tax-mc/Assets/Scripts/Goal/SinText.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class SinText : MonoBehaviour
-{
-    float sinx, cosy;
-
-    private void Update() => transform.localScale = new(Mathf.Sin(Time.time), Mathf.Cos(Time.time));
-}
